6acd29bd006851074719526ebbd7323568bb63d1
majuba <mjbackhaus@gmail.com>
2025-07-06 14:26:01 +0200

    Initialize project with .gitignore and main.py for task-cli

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import os
+import sys 
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser( 
+                prog='task-cli', 
+                description='Simple CLI Todo list'
+                )
+    
