af971753cfca0b1ff536cb17f624bc9f0040aa31
majuba <mjbackhaus@gmail.com>
2025-07-06 18:32:34 +0200

    Implement add command functionality in task-cli

--- src/main.py
+++ src/main.py
@@ -2,9 +2,20 @@ import os
 import sys 
 import argparse
 
+def add_command(args):
+    print(f"add!{ args.item}")
+    return 1
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser( 
                 prog='task-cli', 
                 description='Simple CLI Todo list'
                 )
-    
+    subparsers = parser.add_subparsers(required=True)
+
+    parser_add = subparsers.add_parser("add")
+    parser_add.add_argument('item')
+    parser_add.set_defaults(func=add_command)
+
+    args = parser.parse_args()
+    args.func(args)
