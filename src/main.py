648088f25097df3f3680fcbcd0edec761ba1c19e
majuba <mjbackhaus@gmail.com>
2025-07-10 19:01:59 +0200

    Implemented delete and update

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 
 import argparse
 
-from taskcli import add_command, list_command
+from taskcli import add_command, list_command, delete_command, update_command
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser( 
@@ -10,13 +10,26 @@ if __name__ == '__main__':
                 )
     subparsers = parser.add_subparsers(required=True)
 
+    # add command 
     parser_add = subparsers.add_parser("add")
     parser_add.add_argument('item')
     parser_add.set_defaults(func=add_command)
 
+    # list command
     parser_list = subparsers.add_parser("list")
     parser_list.add_argument("status", nargs="?", choices=["done", "todo", "in-progress"])
     parser_list.set_defaults(func=list_command)
+
+    # delete command 
+    parser_delete = subparsers.add_parser("delete")
+    parser_delete.add_argument("task_id",type=int)
+    parser_delete.set_defaults(func=delete_command)
+
+    # update command 
+    parser_update = subparsers.add_parser("update")
+    parser_update.add_argument("task_id", type=int)
+    parser_update.add_argument("desc")
+    parser_update.set_defaults(func=update_command)
 
     args = parser.parse_args()
     args.func(args)
--- src/taskcli.py
+++ src/taskcli.py
@@ -24,13 +24,27 @@ class TaskList():
     def __init__(self, last_id = 0, tasks = []):
         self.last_id = last_id
         self.tasks = tasks
+    
+    def add_task(self, task):
+        self.tasks.append(task)
 
+    def remove_task(self, task_id):
+        for i in range(len(self.tasks)-1):
+            if self.tasks[i].task_id == task_id:
+                del self.tasks[i] 
+        
     def to_dict(self):
         return {
             "last_id" : self.last_id,
             "tasks" : [t.to_dict() for t in self.tasks] 
         }
-    
+    def update_task(self, task_id, desc):
+        for task in self.tasks:
+            if task.task_id == task_id:
+                task.desc = desc 
+                return 
+        raise ValueError()
+
     @staticmethod
     def from_dict(data):
         tasks = [Task.from_dict(task) for task in data['tasks']]
@@ -51,7 +65,7 @@ def add_command(args):
     tasklist = load_tasklist()
     tasklist.last_id += 1
     new_task = Task(tasklist.last_id, args.item, status="todo")
-    tasklist.tasks.append(new_task)
+    tasklist.add_task(new_task)
     write_tasklist(tasklist)
     print(f"task added successfully (Id: {tasklist.last_id})")
 
@@ -61,3 +75,19 @@ def list_command(args):
         if args.status and task.status != args.status:
             continue
         print(task.desc, f" Id: {task.task_id}")
+
+def delete_command(args):
+    tasklist = load_tasklist()
+    tasklist.remove_task(args.task_id)
+    write_tasklist(tasklist)
+    print(f"Successfully removed task with id {args.task_id}")
+
+def update_command(args):
+    tasklist = load_tasklist()
+    try:
+        tasklist.update_task(args.task_id, args.desc)
+    except ValueError:
+        print(f"No task with id {args.task_id} found!")
+        return
+    write_tasklist(tasklist)
+    print(f"Successfully updated task with id {args.task_id}")
